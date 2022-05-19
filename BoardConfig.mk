3d07ef754d5803d09e8d276ee7f2dcb40e4d501b
FlominatorGD <github.flominatorgd@gmail.com>
2022-05-19 20:20:06 +0200

    j6lte: built ANT+, back to android.hardware.graphics.composer@2.1 , may some more stuff...

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -28,7 +28,7 @@ BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(DEVICE_PATH)/bluetooth
 # Kernel
 TARGET_KERNEL_CONFIG := exynos7870-j6lte_defconfig
 
-# HIDL
+# Device Manifest
 DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/configs/manifest.xml
 
 # Init
